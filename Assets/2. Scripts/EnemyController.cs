c8dd690a3830f47a3dca49a471f838bfe6d6c6d9
Adonis <wintiger0908@gmail.com>
2026-08-09 13:03:13 +0900

    적 제작 2

--- Assets/2. Scripts/EnemyController.cs
+++ Assets/2. Scripts/EnemyController.cs
@@ -1,24 +1,65 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     static string BULLET_TAG = "Bullet";
+    static string STATE = "State";
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Animator animator;
+    [SerializeField] AnimationClip deadClip;
+
+    bool onDead;
+    float time;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        onDead   = false;
+        time     = 0.0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (onDead)
+        {
+            time += Time.deltaTime;
+        }
+        if (time > 0.6f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(BULLET_TAG))
-            Destroy(gameObject);
+        {
+            animator.SetInteger(STATE, 1);
+            OnDead();
+        }
     }
+
+    void OnDead()
+    {
+        onDead = true;
+    }
+
+    #region 코루틴
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag(BULLET_TAG))
+    //    {
+    //        StartCoroutine(OnDead());
+    //    }
+    //}
+
+    //IEnumerator OnDead()
+    //{
+    //    animator.SetInteger(STATE, 1);
+
+    //    yield return new WaitForSeconds(deadClip.length);
+    //    Destroy(gameObject);
+    //}
+    #endregion
 }
